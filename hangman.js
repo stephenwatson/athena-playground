a79f5ab2ac009abd4cf0395070f211b1656fb1d5
Daniel <daniel.sonner@gmail.com>
2013-03-02 15:26:31 -0800

    fixed indents

--- hangman.js
+++ hangman.js
@@ -1,68 +1,68 @@
 var word, gottenRight, i, lettersGuessed, lives, LETTERS;
+// The letters constant is used to creating properties on the lettersGuessed object
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
 // This funtion is called by the HTML page when the submit guess button is clicked
 function letterGuessed(){
-    //change it to upper case so it will match the word even if they enter lowercase
-    var letterGuessed = document.getElementById('guess').value.toUpperCase();
-    updateLetters(letterGuessed);
+  //change it to upper case so it will match the word even if they enter lowercase
+  var letterGuessed = document.getElementById('guess').value.toUpperCase();
+  updateLetters(letterGuessed);
 }
 
 // updates the gottenRight array
 function updateLetters(guess){
-    var correctGuess = false;
-    // Check that guess has a value
-    if (guess && !lettersGuessed[guess]){
-        // Add their guess to the array of guessed letters
-        lettersGuessed[guess] = true;
-        for (i=0; i<word.length; i++)
-        {
-            if(guess.charAt(0) === word.charAt(i)){
-                gottenRight[i] = true;
-                correctGuess = true;
-            }
+  var correctGuess = false;
+  // Check that guess has a value
+  if (guess && !lettersGuessed[guess]){
+    // Add their guess to the array of guessed letters
+    lettersGuessed[guess] = true;
+    for (i=0; i<word.length; i++)
+      {
+        if(guess.charAt(0) === word.charAt(i)){
+          gottenRight[i] = true;
+          correctGuess = true;
+      }
+    }
+    if (!correctGuess){
+      lives--;
+      // Change the image to the new number of lives image
+      document.getElementById('lifeImage').innerHTML = '<img src="' + lives + 'lives.jpg">';
+        if (lives === 0) {
+          // Should disable the button to submit a new letter here
         }
-        if (!correctGuess){
-            lives--;
-            // Change the image to the new number of lives image
-            document.getElementById('lifeImage').innerHTML = 
-              '<img src="' + lives + 'lives.jpg">';
-            if (lives === 0) {
-                // Should disable the button to submit a new letter here
-            }
-        }
-        rewriteWord();
+      }
+      rewriteWord();
     }
 }
 
 // Reloads the word on the HTML page showing letters the user has correctly guess, 
 // otherwise showing underscores
 function rewriteWord(){
-    var reWrittenWord = "";
-    for (i=0; i < gottenRight.length; i++){
-        if (gottenRight[i]){
-            reWrittenWord += word.charAt(i);
-            reWrittenWord += " ";
-        }
-        else if(word.charAt(i) === ' '){
-            reWrittenWord += "&emsp;&emsp;";
-        }
-        else{
-            reWrittenWord += '_';
-            reWrittenWord += " ";
-        }
+  var reWrittenWord = "";
+  for (i=0; i < gottenRight.length; i++){
+    if (gottenRight[i]){
+      reWrittenWord += word.charAt(i);
+      reWrittenWord += " ";
     }
-    var innerHTML ="<h1>"+reWrittenWord+"</h1>";
-    // Write out letters guessed
-    innerHTML += "<br /> Letters Guessed: ";
-    for (var key in lettersGuessed){
-      if (lettersGuessed.hasOwnProperty(key) && lettersGuessed[key]){
-        innerHTML+= key + " ";
-      }
+    else if(word.charAt(i) === ' '){
+      reWrittenWord += "&emsp;&emsp;";
     }
-    document.getElementById('wordDisplay').innerHTML = innerHTML;
+    else{
+      reWrittenWord += '_';
+      reWrittenWord += " ";
+    }
+  }
+  var innerHTML ="<h1>"+reWrittenWord+"</h1>";
+  // Write out letters guessed
+  innerHTML += "<br /> Letters Guessed: ";
+  for (var key in lettersGuessed){
+    if (lettersGuessed.hasOwnProperty(key) && lettersGuessed[key]){
+      innerHTML+= key + " ";
+    }
+  }
+  document.getElementById('wordDisplay').innerHTML = innerHTML;
 }
 
 // This function is called when the replay button is pressed and resets the game.
