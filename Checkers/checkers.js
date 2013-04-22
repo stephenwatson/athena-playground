5460670c2c1abaefdcc48e1f51e7967b7f1b806b
Daniel <daniel.sonner@gmail.com>
2013-04-21 17:10:51 -0700

    made board, added circle CSS

---
+++ Checkers/checkers.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  var squareHTML;
+  // Create the board, i is rows, j is columns
+  for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++) {
+      squareHTML = (((j+i%2)%2===0))  ? '<div id="whiteSquare" style="margin:'+50*i+',0,0, '+j*50+'px;"> </div>' : 
+        '<div id="blackSquare" style="margin:'+50*i+',0,0, '+j*50+'px;"> </div>';
+      $('#board').append(squareHTML);
+    }
+    $('#board').append('<br><br><br>');
+  }
+});
