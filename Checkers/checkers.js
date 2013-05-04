850c4e337af866e293b723476dfe96d0486eae6a
stephenwatson <stephen.watson96@gmail.com>
2013-05-04 15:51:23 -0700

    added in the dots, they are not in the correct spot

--- Checkers/checkers.js
+++ Checkers/checkers.js
@@ -10,4 +10,26 @@ $(document).ready(function(){
     //why does it take so many br's?
     $('#board').append('<br><br><br>');
   }
+  
+       //This puts down the pink pices at the top, I just copied the code making the board
+  for (i = 0; i < 3; i++) {
+   for (j = 0; j < 8; j++) {
+        if ((j+i%2)%2===1)
+        {
+       pinkPiece =  '<div id="pinkPiece" style="margin:'+50*i+',0,0, '+j*50+'px;">';
+       $('#board').append(pinkPiece);
+        }
+   }
+  }
+    // This puts green pieces at the bottom of the board
+  for (i = 5; i < 8; i++) {
+   for (j = 0; j < 8; j++) {
+        if ((j+i%2)%2===1)
+        {
+       greenPiece =  '<div id="greenPiece" style="margin:'+50*i+',0,0, '+j*50+'px;">';
+       $('#board').append(greenPiece);
+        }
+   }
+  }
+
 });
